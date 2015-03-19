1ad2dea89741bbb7bbda3f68f31553a81ca1e17a
Dogiuks <dd622@york.ac.uk>
2015-03-19 11:18:44 +0000

    Refactor

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -14,10 +14,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-
-
-
 void MainWindow::on_cgpButton_clicked()
 {
 
@@ -55,58 +51,64 @@ void MainWindow::on_actionOpen_triggered()
 
 void MainWindow::on_buttonHome_clicked()
 {
-    int red;
-    int green;
-    int blue;
 
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
-            int num = 1+(74*output[i+10*j].demand/100);
-            if(num>60)
-            {
-                red = 255;
-                green = 0;
-                blue = 224+(num-75)*16;
-            }
-            else if(num>35)
-            {
-                red = 255;
-                green = 10*(61-num);
-                blue = 0;
-            }
-            else if(num>28)
-            {
-                    red = 253-38*(35-num);
-                    green = 255;
-                    blue = 0;
-            }
-            else if(num>21)
-            {
-                    red = 0;
-                    green = 255;
-                    blue = 16+38*(28-num);
-            }
-            else if(num>6)
-            {
-                    red = 0;
-                    green = 228-16*(21-num);
-                    blue = 255;
-            }
-            else if(num>0)
-            {
-                red = 6-num;
-                green = 0;
-                blue = 255;
-            }
-            else
-            {
-                red = 0;
-                green = 0;
-                blue = 0;
-            }
-            ui->warehouseWiew->item(j,i)->setBackgroundColor(QColor(red, green, blue, 255));
-            ui->warehouseWiew->item(j,i)->setText(QString::number(num));
+
+            ui->warehouseWiew->item(j,i)->setBackgroundColor(getGradient(output[i+10*j].demand, 100));
+            ui->warehouseWiew->item(j,i)->setText(QString::number(output[i+10*j].demand));
             ui->warehouseWiew->item(j,i)->setTextColor(QColor(Qt::white));
         }
     }
+}
+QColor MainWindow::getGradient( int atPoint, int max)
+{
+    int red;
+    int green;
+    int blue;
+    int num = 1+(74*atPoint/max);
+
+    if(num>60)
+    {
+        red = 255;
+        green = 0;
+        blue = 224+(num-75)*16;
+    }
+    else if(num>35)
+    {
+        red = 255;
+        green = 10*(61-num);
+        blue = 0;
+    }
+    else if(num>28)
+    {
+            red = 253-38*(35-num);
+            green = 255;
+            blue = 0;
+    }
+    else if(num>21)
+    {
+            red = 0;
+            green = 255;
+            blue = 16+38*(28-num);
+    }
+    else if(num>6)
+    {
+            red = 0;
+            green = 228-16*(21-num);
+            blue = 255;
+    }
+    else if(num>0)
+    {
+        red = 6-num;
+        green = 0;
+        blue = 255;
+    }
+    else
+    {
+        red = 0;
+        green = 0;
+        blue = 0;
+    }
+    return QColor(red, green, blue, 255);
 }
--- GUI/mainwindow.h
+++ GUI/mainwindow.h
@@ -11,6 +11,8 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    QColor getGradient( int atPoint, int max);
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
